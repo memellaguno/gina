df1d8402c0f7db543475042cf4305815aaa743a0
Familia Cobos Medel <arkangeldigital@gmail.com>
2026-02-05 19:49:23 -0600

    Angel cta changes 3

--- components/blocks/CtaBanner.tsx
+++ components/blocks/CtaBanner.tsx
@@ -24,14 +24,14 @@ export default function CtaBanner({ block, lang = "es" }: Props) {
   const style = stegaClean(block.style) || "speaking";
 
   const styleClasses = {
-    speaking: "bg-primary text-white py-24 text-center",
-    newsletter: "section bg-secondary-child text-white py-8 text-center",
-    quote: "bg-muted text-secondary py-24 text-center",
+    speaking: "w-full px-8 bg-primary text-white py-24 text-center",
+    newsletter: "w-full px-8 section bg-secondary-child text-white py-8 text-center",
+    quote: "w-full px-8 bg-muted text-secondary py-24 text-center",
     contact: "bg-muted text-primary py-24",
   };
 
   return (
-    <section className={cn("w-full px-8", styleClasses[style as keyof typeof styleClasses])}>
+    <section className={cn("", styleClasses[style as keyof typeof styleClasses])}>
       <div className="mx-auto flex w-full max-w-[1900px] flex-col items-center ">
         {eyebrow && style !== "quote" && (
           <p className="mb-4 text-sm font-medium uppercase lg:text-base reveal">
--- components/blocks/Form.tsx
+++ components/blocks/Form.tsx
@@ -21,36 +21,50 @@ export default function Form({ block }: FormProps) {
   if (!block) return null;
 
   return (
-    <div className="container px-4 py-6 md:px-8 md:py-10">
+    <div >
       {block.heading && (
         <div className="text-xl md:text-2xl">{block.heading}</div>
       )}
 
       {block.text && (
-        <div className="mt-2 text-sm md:mt-4 md:text-base">{block.text}</div>
+        <div className="md:text-1xl lg:text-2xl mb-8">{block.text}</div>
       )}
 
       {block.items && block.items.length > 0 && (
-        <div className="mt-4 space-y-4 md:mt-6 md:space-y-6">
+        <div className="mt-12 space-y-6 md:mt-12 md:space-y-8">
           {block.items.map((item) => {
             return (
               <div key={item._key} className="flex flex-col gap-2">
                 {item.title && (
-                  <div className="text-sm font-medium md:text-base">
+                  <div className="md:text-1xl lg:text-2xl font-medium uppercase">
                     {item.title}
                   </div>
                 )}
 
-                {item.placeholder && (
+                {item.placeholder && item.type != "message" && (
+
                   <input
                     type={item.type || "text"}
                     placeholder={item.placeholder}
-                    className="rounded-md border border-gray-300 px-3 py-2 text-sm md:text-base"
+                    className="md:text-1xl lg:text-2xl bg-transparent border-b"
                   />
                 )}
+
+                {item.placeholder && item.type == "message" && (
+                  <textarea
+                    placeholder={item.placeholder}
+                    className="md:text-1xl lg:text-2xl bg-transparent border-b"
+                  />
+                )}
+
               </div>
             );
           })}
+          <div className="flex justify-end">
+            <button type="submit" className="send-btn text-primary mb-14 p-2 w-72 hover:bg-primary hover:text-white">SEND <svg width="18" height="11" viewBox="0 0 18 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M17 5.31836L12.8681 10.3184M17 5.31836L12.8681 0.318359M17 5.31836L2.5828e-07 5.31836" stroke="#060F9F"/></svg>
+            </button>
+          </div>
         </div>
       )}
     </div>
